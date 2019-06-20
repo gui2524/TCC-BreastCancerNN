6eb4d259f1173d0cbee79000ffe8544f39f4515b
gui2524 <garaujo@stone.com.br>
2019-06-20 20:56:04 -0300

    Alterando scripts e adicionando csv de test

--- Projeto 2/scripts/convertDicomToPng.py
+++ Projeto 2/scripts/convertDicomToPng.py
@@ -2,22 +2,31 @@ import pydicom as dicom
 import os
 import cv2
 import glob
-# make it True if you want in PNG format
-PNG = True
+
+txt = input("Type [1] for TRAIN or [2] for PREDICT: ") 
+
+
+if txt == '1':
+    folder = "TRAIN"
+elif txt == '2':
+    folder = "PREDICT"
+else:
+    print("Invalid option: " + txt)
+    exit()
+    
 # Specify the .dcm folder path
-folder_path = "C:\\Users\\Guilherme\\Desktop\\IMGMEDICAS\\TRAIN\\CBIS-DDSM"
+folder_path = "C:\\Users\\Guilherme\\Desktop\\IMGMEDICAS\\" + folder + "\\CBIS-DDSM"
 # Specify the output jpg/png folder path
 jpg_folder_path = "C:\\Users\\Guilherme\\Desktop\\IMGMEDICAS\\JPG"
 images_path = os.listdir(folder_path)
 
-wildcard = r'C:/Users/Guilherme/Desktop/IMGMEDICAS/TRAIN/CBIS-DDSM/*/*/*/*'
+wildcard = r'C:/Users/Guilherme/Desktop/IMGMEDICAS/' + folder + '/CBIS-DDSM/*/*/*/*0.dcm'
 i = 0
 for file in glob.glob(wildcard):
-    ds = dicom.dcmread(file, force=True)
-    pixel_array_numpy = ds.pixel_array
-    image = file.replace('.dcm', str(i) + '.png')
-    cv2.imwrite(os.path.join(jpg_folder_path, image), pixel_array_numpy)
-    print(i)
-    i+=1
+        ds = dicom.dcmread(file, force=True)
+        pixel_array_numpy = ds.pixel_array
+        image = file.replace('.dcm', str(i) + '.png')
+        cv2.imwrite(os.path.join(jpg_folder_path, image), pixel_array_numpy)
+        print(i)
 
 
--- Projeto 2/scripts/readCsv.py
+++ Projeto 2/scripts/readCsv.py
@@ -2,24 +2,39 @@ import csv
 import glob
 import shutil
 
-with open('C:\\Users\\Guilherme\\Desktop\\TCC\\TCC-BreastCancerNN\\Projeto 2\\resources\\data_info\\calc_case_description_train_set.csv') as f: 
-    reader = csv.reader(f)
-    your_list = list(reader)
+txt = input("Type [1] for TRAIN or [2] for PREDICT: ") 
+
+
+if txt == '1':
+    folder = "TRAIN"
+    with open('C:\\Users\\Guilherme\\Desktop\\TCC\\TCC-BreastCancerNN\\Projeto 2\\resources\\data_info\\calc_case_description_train_set.csv') as f: 
+        reader = csv.reader(f)
+        your_list = list(reader)
+elif txt == '2':
+    folder = "PREDICT"
+    with open('C:\\Users\\Guilherme\\Desktop\\TCC\\TCC-BreastCancerNN\\Projeto 2\\resources\\data_info\\calc_case_description_test_set.csv') as f: 
+        reader = csv.reader(f)
+        your_list = list(reader)
+else:
+    print("Invalid option: " + txt)
+    exit()
     
     
-mal_dest_dir = "C:\\Users\\Guilherme\\Desktop\\IMGMEDICAS\\TRAIN_DIV\\MALIGNANT\\"
-ben_dest_dir = "C:\\Users\\Guilherme\\Desktop\\IMGMEDICAS\\TRAIN_DIV\\BENIGN\\"  
-ben_cb_dest_dir = "C:\\Users\\Guilherme\\Desktop\\IMGMEDICAS\\TRAIN_DIV\\BENIGN_WO_CB\\"      
+mal_dest_dir = "C:\\Users\\Guilherme\\Desktop\\IMGMEDICAS\\" + folder + "_DIV\\MALIGNANT\\"
+ben_dest_dir = "C:\\Users\\Guilherme\\Desktop\\IMGMEDICAS\\" + folder + "_DIV\\BENIGN\\"  
+ben_cb_dest_dir = "C:\\Users\\Guilherme\\Desktop\\IMGMEDICAS\\" + folder + "_DIV\\BENIGN_WO_CB\\" 
+i = 0     
 for item in your_list:
-    wildcard = r'C:/Users/Guilherme/Desktop/IMGMEDICAS/TRAIN/CBIS-DDSM/' + '*' + str(item[0]) + '*' +'/*/*/*.png'
-    for file in glob.glob(wildcard):
-        if item[9] == 'MALIGNANT':
-            dest_dir = mal_dest_dir
-        elif item[9] == 'BENIGN':
-            dest_dir = ben_dest_dir
-        elif item[9] == 'BENIGN_WITHOUT_CALLBACK':
-            dest_dir = ben_cb_dest_dir
+        wildcard = r'C:/Users/Guilherme/Desktop/IMGMEDICAS/' + folder +'/CBIS-DDSM/' + '*' + str(item[0]) + '*' +'/*/*/*.png'
+        for file in glob.glob(wildcard):
+            if item[9] == 'MALIGNANT':
+                dest_dir = mal_dest_dir
+            elif item[9] == 'BENIGN':
+                dest_dir = ben_dest_dir
+            elif item[9] == 'BENIGN_WITHOUT_CALLBACK':
+                dest_dir = ben_cb_dest_dir
         
-        print(file + " copied")
-        shutil.move(file, dest_dir)
+            print(file + " moved")
+            shutil.move(file, dest_dir)
+
 
